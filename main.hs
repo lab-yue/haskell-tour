5ea8b3c95bc8e08f2c651d594ecccc06d7e1bf30
rainy-me <github@rainy.me>
2019-12-22 16:06:35 +0900

    init

---
+++ main.hs
@@ -0,0 +1,18 @@
+-- comment
+{-
+   test comment
+-}
+
+import Data.List
+import System.IO
+
+-- max int
+maxInt = maxBound :: Int
+bigFloat = 3.9999999999999 + 0.9999999999999 
+
+a :: Int
+a = 1
+
+-- sum
+
+sumOfVals = sum [1...1000]
